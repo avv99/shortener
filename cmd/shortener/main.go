b7e258d7b8319de82add1823ebd62ec71d347a19
avv99 <72482107+avv99@users.noreply.github.com>
2024-02-19 11:46:50 +0700

    123321

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,44 +2,63 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+)
+
+var (
+	shortURLs map[string]string // Мапа для хранения сокращенных URL
+	baseURL   = "http://localhost:8080"
 )
 
 func main() {
-	http.HandleFunc("/", handlePost)
-	http.HandleFunc("/{id}", handleGet)
-	fmt.Println("Server is running at http://localhost:8080")
+	shortURLs = make(map[string]string)
+
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/shorten", handleShorten)
+	http.HandleFunc("/redirect/", handleRedirect)
+
+	fmt.Println("Server is running at", baseURL)
 	http.ListenAndServe(":8080", nil)
 }
 
-func handlePost(w http.ResponseWriter, r *http.Request) {
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Invalid request", http.StatusBadRequest)
+}
+
+func handleShorten(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	// Чтение строки URL из тела запроса
-	url := r.FormValue("url")
-	if url == "" {
+	r.ParseForm()
+	longURL := r.Form.Get("url")
+	if longURL == "" {
 		http.Error(w, "URL is required", http.StatusBadRequest)
 		return
 	}
 
-	// Ваша логика для сокращения URL
-	shortenedURL := shortenURL(url)
+	shortID := generateShortID()
+	shortURL := baseURL + "/redirect/" + shortID
+	shortURLs[shortID] = longURL
 
-	// Отправка ответа с сокращенным URL
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, shortenedURL)
+	fmt.Fprintf(w, shortURL)
 }
 
-// Ваша логика для сокращения URL
-func shortenURL(url string) string {
-	// Ваш код для сокращения URL
-	return "shortened-url"
+func handleRedirect(w http.ResponseWriter, r *http.Request) {
+	shortID := r.URL.Path[len("/redirect/"):]
+	longURL, ok := shortURLs[shortID]
+	if !ok {
+		http.Error(w, "Invalid short URL", http.StatusBadRequest)
+		return
+	}
+
+	http.Redirect(w, r, longURL, http.StatusTemporaryRedirect)
 }
 
-func handleGet(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Path[len("/"):]
-	fmt.Fprintf(w, "This is GET /%s endpoint", id)
+func generateShortID() string {
+	return strconv.Itoa(rand.Intn(1000000)) // Генерация случайного числа в диапазоне [0, 1000000)
 }
