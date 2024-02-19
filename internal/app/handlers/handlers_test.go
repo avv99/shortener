ecb895f550fe1ebd26d320398e4008ed35372fac
avv99 <72482107+avv99@users.noreply.github.com>
2024-02-19 21:53:44 +0700

    123123

--- internal/app/handlers/handlers_test.go
+++ internal/app/handlers/handlers_test.go
@@ -1,1 +1,53 @@
 package handlers
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAddItem(t *testing.T) {
+	// Создание тестового запроса
+	requestBody := []byte("http://example.com")
+	req, err := http.NewRequest("POST", "/", bytes.NewBuffer(requestBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Создание mock HTTP ResponseWriter
+	w := httptest.NewRecorder()
+
+	// Вызов функции
+	AddItem(w, req)
+
+	// Проверка статуса кода
+	if status := w.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
+	}
+
+	// Проверка корректности ответа
+	expectedResponse := "http://localhost:8080/1"
+	if w.Body.String() != expectedResponse {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			w.Body.String(), expectedResponse)
+	}
+
+	// Проверка добавления элемента
+	if len(shortenedURLs) != 1 {
+		t.Errorf("handler didn't add item to shortenedURLs slice")
+	}
+
+	// Проверка данных добавленного элемента
+	addedItem := shortenedURLs[0]
+	if addedItem.ID != 1 {
+		t.Errorf("added item has wrong ID: got %v want %v", addedItem.ID, 1)
+	}
+	if addedItem.Original != "http://example.com" {
+		t.Errorf("added item has wrong Original URL: got %v want %v", addedItem.Original, "http://example.com")
+	}
+	if addedItem.Shortened != expectedResponse {
+		t.Errorf("added item has wrong Shortened URL: got %v want %v", addedItem.Shortened, expectedResponse)
+	}
+}
