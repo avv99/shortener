4d8eb2ac061f71c438edf2abb4f31b6954ffee56
avv99 <72482107+avv99@users.noreply.github.com>
2024-02-26 16:15:18 +0700

    123

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"net/http"
-	"os" // Импортируем пакет для работы с переменными окружения
+	"os" // Импортируем пакет для работы с переменными окружения1
 	"shortener/internal/app/handlers"
 )
 
